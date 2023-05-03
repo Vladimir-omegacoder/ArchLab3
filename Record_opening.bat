df9375bcbb386ffed2aef3a50561cfedaa28a5a2
Vladimir-omegacoder <65459308+Vladimir-omegacoder@users.noreply.github.com>
2023-05-03 16:44:03 +0300

    Yet another task in Pt1
    Here you can see how I completed another task in Pt1.

---
+++ Record_opening.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+goto :main
+
+
+:main
+setlocal
+
+echo Record opened...
+
+C:\Users\KillMe\Record.docx
+
+echo Record closed...
+
+pause
+
+endlocal
+goto :eof
